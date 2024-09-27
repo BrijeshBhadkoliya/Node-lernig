ee3eabc3e8f3c41ddf5b4902d406658e21f6465e
Brijesh Bhadkoliya <bhadkoliyajbrijesh@gmail.com>
2024-09-27 18:43:38 +0530

    hosting-url-server

---
+++ Server-core-modul/host-1.js
@@ -0,0 +1,17 @@
+const { createServer } = require("node:http");
+
+const hostname = "127.0.0.1";
+
+const port = 3002;
+
+const server = createServer((req, res) => {
+    console.log(req.url);
+    
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
